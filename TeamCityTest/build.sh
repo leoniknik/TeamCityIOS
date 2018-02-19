54e25005f3e647068e78f4e731ae5afe9b941cea
VolodinK <VolodinK@binbank.ru>
2018-02-19 11:28:53 +0300

    slash

---
+++ TeamCityTest/build.sh
@@ -0,0 +1,11 @@
+rm -rf ~/Library/Developer/Xcode/DerivedData/TeamCityTest
+xcrun simctl erase all
+# Unit tests
+xctool \
+-scheme TeamCityTestUITests \
+-workspace TeamCiryTest.xcworkspace \
+-destination "platform=iOS Simulator,name=iPhone 8,OS=10.0" \
+-sdk iphonesimulator \
+-reporter junit:test-results/TEST-UnitTests.xml \
+-reporter pretty \
+clean test
