eb55f1dc91cf12bc00f3f3b94c017612663d1773
VolodinK <VolodinK@binbank.ru>
2018-02-19 12:21:43 +0300

    f

--- TeamCityTest/build.sh
+++ TeamCityTest/build.sh
@@ -3,7 +3,7 @@ rm -rf ~/Library/Developer/Xcode/DerivedData/TeamCityTest
 xcrun simctl erase all
 pod install
 # Unit tests
-xctool \
+xcodebuild \
 -scheme TeamCityUnit \
 -workspace TeamCityTest.xcworkspace \
 -destination "platform=iOS Simulator,name=iPhone 8,OS=11.2" \
