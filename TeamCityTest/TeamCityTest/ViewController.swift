c3f30f09b68378fca445508ab1c224c4f42a2434
VolodinK <VolodinK@binbank.ru>
2018-02-19 11:19:18 +0300

    ddd

---
+++ TeamCityTest/TeamCityTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TeamCityTest
+//
+//  Created by Кирилл Володин on 19.02.2018.
+//  Copyright © 2018 Кирилл Володин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
